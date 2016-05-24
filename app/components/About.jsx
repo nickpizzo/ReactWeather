9b0d9bfb4930bc051809b58c39eb50ba6692e05f
Nick Pizzo <nick.pizzo@gmail.com>
2016-05-23 21:55:40 -0700

    add styles to weather page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,13 +2,12 @@ var React = require('react');
 
 // this refactor only availbale becuase this component doesnt touch state
 
-
-
 var About = (props) => {
   return (
     <div>
-      <h3>about component</h3>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">about component</h1>
+      <p>This is a weather application built on React</p>
+      <p>Tools used to build this app</p>
     </div>
   )
 };
