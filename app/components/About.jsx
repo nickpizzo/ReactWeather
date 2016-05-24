b09a56717b5eb0b24ab83e312c62381e123ef2ce
Nick Pizzo <nick.pizzo@gmail.com>
2016-05-24 00:59:18 -0700

    added custom css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,7 +5,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">about component</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>This is a weather application built on React</p>
       <p>Tools used to build this app</p>
     </div>
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -4,7 +4,7 @@ var {Link} = require('react-router');
 var Examples = (props) => {
   return (
     <div>
-    <h1 className="text-center">examples</h1>
+    <h1 className="text-center page-title">Examples</h1>
     <p>Example locations</p>
     <ol>
       <li>
